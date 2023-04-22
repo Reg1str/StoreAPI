d63b18372c0bd43729db987f1a902970b3db9ed4
Reg1str <ilya.n1konov@inbox.ru>
2023-04-22 11:24:36 +0300

    Logging in category controller

--- StoreAPI/Controllers/CategoryController.cs
+++ StoreAPI/Controllers/CategoryController.cs
@@ -20,8 +20,12 @@ public class CategoryController : ControllerBase
     public async Task<ActionResult<List<Category>>> AddCategory (Category newCategory)
     {
         _repository.AddCategory(newCategory);
+        _logger.LogInformation("{Now}: Start writing product to data base",
+            new DateTime(638177575191726912L, DateTimeKind.Local));
         if (await _repository.SaveChangesAsync())
         {
+            _logger.LogInformation("{Now}: Finished writing product to data base",
+                new DateTime(638177575071869031L, DateTimeKind.Local));
             var result = _repository.GetAllCategories();
             return Ok(result);
         }
@@ -33,6 +37,8 @@ public class CategoryController : ControllerBase
     public Task<ActionResult<List<Category>>> GetAllCategories()
     {
         var result = _repository.GetAllCategories();
+        _logger.LogInformation("{Now}: Getting all categories from data base",
+            new DateTime(638177575191726912L, DateTimeKind.Local));
         
         return  Task.FromResult<ActionResult<List<Category>>>(Ok(result));
     }
@@ -41,6 +47,8 @@ public class CategoryController : ControllerBase
     public Task<ActionResult<Category>> GetCategory(int id)
     {
         var result = _repository.GetCategory(id);
+        _logger.LogInformation("{Now}: Getting from data base category with id {Id} ",
+            new DateTime(638177575191726912L, DateTimeKind.Local), id.ToString());
         
         return Task.FromResult<ActionResult<Category>>(Ok(result));
     }
