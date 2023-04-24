feeb1ba9160ac86ed753019d066b241c4c7023cb
Reg1str <ilya.n1konov@inbox.ru>
2023-04-24 16:25:49 +0300

    Brand controller first version

--- StoreAPI/Controllers/BrandController.cs
+++ StoreAPI/Controllers/BrandController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoreAPI.Database.Repository;
+using StoreAPI.Models;
 
 namespace StoreAPI.Controllers;
 
@@ -7,9 +9,51 @@ namespace StoreAPI.Controllers;
 public class BrandController : ControllerBase
 {
     private readonly ILogger<BrandController> _logger;
+    private readonly IRepository _repository;
 
-    public BrandController(ILogger<BrandController> logger)
+    public BrandController(ILogger<BrandController> logger, IRepository repository)
     {
         _logger = logger;
+        _repository = repository;
+    }
+    
+    [HttpPost("add", Name = "AddBrand")]
+    public async Task<ActionResult<List<Brand>>> AddBrand (Brand newBrand)
+    {
+        _repository.AddBrand(newBrand);
+        _logger.LogInformation("{Now}: Writing brand to data base",
+            new DateTime(638177575191726912L, DateTimeKind.Local));
+        if (await _repository.SaveChangesAsync())
+        {
+            _logger.LogInformation("{Now}: Successful write brand to data base",
+                new DateTime(638177575071869031L, DateTimeKind.Local));
+            var result = _repository.GetAllBrands();
+            return Ok(result);
+        }
+        
+        _logger.LogInformation("{Now}: Error of saving data",
+            new DateTime(638177575191726912L, DateTimeKind.Local));
+
+        throw new Exception("Error of saving data");
+    }
+    
+    [HttpGet("all", Name = "GetAllBrands")]
+    public Task<ActionResult<List<Brand>>> GetAllBrands()
+    {
+        var result = _repository.GetAllBrands();
+        _logger.LogInformation("{Now}: Getting all brands from data base",
+            new DateTime(638177575191726912L, DateTimeKind.Local));
+        
+        return  Task.FromResult<ActionResult<List<Brand>>>(Ok(result));
+    }
+    
+    [HttpGet("{id}", Name = "GetBrand")]
+    public Task<ActionResult<Brand>> GetBrand(int id)
+    {
+        var result = _repository.GetBrand(id);
+        _logger.LogInformation("{Now}: Getting from data base brand with id:{Id}",
+            new DateTime(638177575191726912L, DateTimeKind.Local), id.ToString());
+        
+        return Task.FromResult<ActionResult<Brand>>(Ok(result));
     }
 }
