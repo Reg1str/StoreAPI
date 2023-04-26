45b1f4c615438188fc6861d49ef4fdd66bacdbfa
Reg1str <ilya.n1konov@inbox.ru>
2023-04-26 20:30:06 +0300

    Register auth service

--- StoreAPI/Program.cs
+++ StoreAPI/Program.cs
@@ -1,5 +1,6 @@
 using StoreAPI.Database;
 using StoreAPI.Database.Repository;
+using StoreAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
