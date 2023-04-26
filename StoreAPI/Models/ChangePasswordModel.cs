3f7ed0ca08b83db6fe4a05b0bd53ed1aeae4a426
Reg1str <ilya.n1konov@inbox.ru>
2023-04-26 19:59:17 +0300

    Auth models

---
+++ StoreAPI/Models/ChangePasswordModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StoreAPI.Models;
+
+public class ChangePasswordModel
+{
+    public string NewPassword { get; set; } = null!;
+    public string ConfirmNewPassword { get; set; } = null!;
+}
---
+++ StoreAPI/Models/RegisterModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StoreAPI.Models;
+
+public class RegisterModel
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
